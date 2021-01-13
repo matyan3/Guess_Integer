92b667726164a1904245ffc324abafe998257fe9
matyan3 <77383369+matyan3@users.noreply.github.com>
2021-01-13 12:56:21 +0100

    First whole code
    Finished adding all needed methods
    Haven't found out how to access text from an Entry created in one method in another one
    
    ----STILL AIN'T FULLY WORKING----

--- Guess.py
+++ Guess.py
@@ -4,38 +4,9 @@ from random import randint as randomize
 from time import sleep
 
 lower, higher = 0, 1000
-
-#method for after user's guess (compares input to the pregenerated number)
-def guess():
-    try:
-        if int(gn.get()) == number:
-            mb.showinfo("Exactly", "That's the number I was thinking")
-        elif gn.get() > number:
-            mb.showinfo("Lower", "My number is lower than " + str(gn.get()))
-        else:
-            mb.showinfo("Bigger", "Oh, I'm thinking of a higher number than " + str(gn.get()))
-    except ValueError:
-        mb.showwarning("Integers only!", "IDC if you entered a float or a string, gimme an integer")
-    except:
-        mb.showerror("Error", "Wild Error appeared (just report to the creator)")
-
-#method for bot's guess
-#creates a dialog with the guessed number and three buttons(Lower, Higher and This one) for the user to reply to the guess
-def botguess():
-    global lower, higher
-    dialog = Tk()
-    dialog.title("Bot guessed")
-    guessednum = Label(dialog, text = str(round((lower+higher)/2)))
-    guessednum.pack()
-    info = Label(tk, text = "My nuber is: ")
-    info.pack()
-    lower = Button(dialog, text = "Lower", command = toohigh)
-    lower.pack()
-    higher = Button(dialog, text = "Higher", command = toolow)
-    higher.pack()
-    exact = Button(dialog, text = "This one", command = celebrate)
-    exact.pack()
-    dialog.mainloop()
+number = 0
+tk = Tk()
+tk.title("Guess Integer")
 
 #upon pressing the "Lower" button
 def toohigh():
@@ -61,3 +32,74 @@ def celebrate():
         startgame()
     else:
         mb.showinfo("Bye then", "Goodbye, loser")
+
+#method for bot's guess
+#creates a dialog with the guessed number and three buttons(Lower, Higher and This one) for the user to reply to the guess
+def botguess():
+    global lower, higher
+    dialog = Tk()
+    dialog.title("Bot guessed")
+    guessednum = Label(dialog, text = str(round((lower+higher)/2)))
+    guessednum.pack()
+    info = Label(tk, text = "My nuber is: ")
+    info.pack()
+    lower = Button(dialog, text = "Lower", command = toohigh)
+    lower.pack()
+    higher = Button(dialog, text = "Higher", command = toolow)
+    higher.pack()
+    exact = Button(dialog, text = "This one", command = celebrate)
+    exact.pack()
+    dialog.mainloop()
+
+#method for after user's guess (compares input to the pregenerated number)
+def guess():
+    global number
+    global widgetNames
+   # try:
+    if int(guessednum.get()) == number:
+        mb.showinfo("Exactly", "That's the number I was thinking")
+    elif guessednum.get() > number:
+        mb.showinfo("Lower", "My number is lower than " + str(gn.get()))
+    else:
+        mb.showinfo("Bigger", "Oh, I'm thinking of a higher number than " + str(gn.get()))
+   # except ValueError:
+   #     mb.showwarning("Integers only!", "IDC if you entered a float or a string, gimme an integer")
+   # except:
+   #     mb.showerror("Error", "Wild Error appeared (just report to the creator)")
+
+#when player clicks the give up button
+def giveup():
+    if mb.askyesno("Give up?", "Do you really wanna give up?"):
+        if mb.askyesno("Play again?", "Shall we start a new game?"):
+            startgame()
+        else:
+            mb.showinfo("Bye then", "Goodbye, loser")
+    else:
+        pass
+
+#when player wants to guess
+def startplayer():
+    global number
+    global widgetNames
+    number = randomize(1, 1000)
+    label = Label(tk, text = "Enter a number (integer) below:")
+    label.pack()
+    guessednum = Entry(tk)
+    guessednum.pack()
+    guessbutton = Button(tk, text = "Submit guess", command = guess)
+    guessbutton.pack()
+    giveupbutton = Button(tk, text = "Give up", command = giveup)
+    giveupbutton.pack()
+    guessednum.focus_set()
+
+#what happens after the launch and in case of restart
+def startgame():
+    if mb.askyesno("Who's gonna guess?", "Will you be the guesser?"):
+        startplayer()
+        tk.mainloop()
+    else:
+        mb.showinfo("Think of a number", "Think of a number, when you have it, press ok")
+        sleep(1)
+        botguess()
+
+startgame()
